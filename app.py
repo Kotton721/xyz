10ae830298c92080ddb3afe563b4c29c945dbfec
xyz-kumikomi <wool21696@gmail.com>
2022-09-26 12:53:05 +0900

    first

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from hashlib import new
 from importlib.resources import path
+from flask import Flask,render_template,send_from_directory
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
